62f4acf23ee1ce52bd78ecf977ed6fe13e5f7b06
Hugo Quiñónez <hugo_2555@hotmail.com>
2018-09-06 13:28:51 -0600

    animation home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, ElementRef, OnInit } from "@angular/core";
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+  animations: [
+    trigger("scrollAnimation", [
+      state(
+        "show",
+        style({
+          opacity: 1,
+          transform: "translateY(0)"
+        })
+      ),
+      state(
+        "hide",
+        style({
+          opacity: 0,
+          transform: "translateY(10%)"
+        })
+      ),
+      transition("show => hide", animate("700ms ease-out")),
+      transition("hide => show", animate("700ms ease-in"))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+  constructor(public el: ElementRef) {}
+  state = "hide";
 
-  constructor() { }
+  ngOnInit() {}
 
+  @HostListener("window:scroll", ["$event"])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    // const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
 
-  total = 2 + 2;
-
-  ngOnInit() {
+    if (scrollPosition >= componentPosition) {
+      this.state = "show";
+    } else {
+      // this.state = "hide";
+    }
   }
-
 }
